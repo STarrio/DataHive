9abe1a6137d67e834b844a028a8a21e242cfc918
Santiago Tarrio <tarriosantiago@gmail.com>
2018-06-14 14:58:19 +0200

    fixed UCIScraper, added file scraping

--- scraping_engine/UCIScraper.py
+++ scraping_engine/UCIScraper.py
@@ -1,5 +1,18 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import extension_dict
+
+IGNORED_DATASETS = ["UJI Pen Characters (Version 2)",
+                                           "Prodigy",
+                                           "Reuters Transcribed Subset",
+                                           "UJIIndoorLoc-Mag",
+                                           "Dishonest Internet users Dataset",
+                                           "APS Failure at Scania Trucks",
+                                           "IDA2016Challenge",
+                                           "chestnut – LARVIC",
+                                           "Early biomarkers of Parkinson's disease based on natural connected speech",
+                                           "Improved Spiral Test Using Digitized Graphics Tablet for Monitoring Parkinson’s Disease",
+                                           "Mesothelioma’s disease data set"]
 
 class UCIScraper:
 
@@ -8,12 +21,13 @@ class UCIScraper:
     def __init__(self):
         self.base_url = "http://archive.ics.uci.edu/ml/"
         self.table_url = "datasets/?format=&task=&att=&area=&numAtt=&numIns=&type=&sort=nameUp&view=table"
-        self.repo = 0 # UCI
+        self.repo = 2 # UCI
+        self.extension_dict = extension_dict.ext_dict
 
     def get_data(self):
         # Retrieves data from /datasets.html
         f = urllib.request.urlopen(self.base_url+self.table_url)
-        bs = BeautifulSoup(f,"lxml")
+        bs = BeautifulSoup(f,"html.parser")
         return bs.find_all("table")
 
     def get_ds(self,path):
@@ -21,6 +35,12 @@ class UCIScraper:
         f = urllib.request.urlopen(self.base_url+path)
         bs = BeautifulSoup(f,'lxml')
         return bs.find_all("table")[1]
+
+    def get_files(self,path):
+        # Retrieves data from dataset's files
+        f = urllib.request.urlopen(self.base_url+path)
+        bs = BeautifulSoup(f,'lxml')
+        return bs.table.contents[7:-3]
 
     def scrape_data(self):
 
@@ -37,6 +57,7 @@ class UCIScraper:
         l = self.get_data()
         l_ds=l[1].find_all("table")[3].find_all("tr")[1:]
         entries = []
+        files = []
         for i in range(len(l_ds)//2):
             entry = {}
             ds_soup = l_ds[2*i]
@@ -44,7 +65,7 @@ class UCIScraper:
             name = ds_soup.contents[1].table.tr.contents[1].p.string
 
             # Ignore troublesome dataset with unscrapeable description
-            if(name=="UJI Pen Characters (Version 2)"):
+            if(name in IGNORED_DATASETS):
                 continue
 
             entry["url"] = self.base_url + path
@@ -52,22 +73,43 @@ class UCIScraper:
             entry["no_instances"] = ds_soup.contents[11].p.string.replace('\xa0','') if len(ds_soup.contents)>11 else ""
             entry["no_attr"] = ds_soup.contents[13].p.string.replace('\xa0','') if len(ds_soup.contents)>13 else ""
 
-            #print(entry["name"])
-
             # scraping over url with get_ds(url)
             detail = self.get_ds(path)
-            download_url = detail.table.span.find_next_siblings()[1].contents[2]['href'][3:]
+            try:
+                download_url = detail.table.span.find_next_siblings()[1].contents[2]['href'][3:]
+            except:
+                print("IGNORE THIS ONE")
+                continue
             abstract = detail.p.find_next_sibling().contents[1][2:]
             area = detail.table.find_next_sibling().find_all("tr")[0].find_all("td")[-1].p.string
-            info = "".join([s if type(s)==str else s.string for s in detail.find_all("p",class_="small-heading")[1].find_next_sibling().contents if len(s)>6])
+            description = "".join([s if type(s)==str else s.string for s in detail.find_all("p",class_="small-heading")[1].find_next_sibling().contents if len(s)>6])
 
             entry["files_download_url"] = self.base_url + download_url
             entry["abstract"] = abstract
             entry["category"] = area
-            entry["info"] = info
+            entry["description"] = description
 
+            files = self.get_files(download_url)
+
+            files_data = [(f.td.find_next_siblings()[0].a['href'],f.td.find_next_siblings()[2].string) for f in files if f!='\n']
+
+            dataset_files = []
+
+            for f in files_data:
+                file_id = f[0]
+                file_name = f[0]
+                file_size = f[1]
+
+                file_name_separated = file_id.split(".")
+                file_type = self.extension_dict[file_name_separated[-1]] if len(file_name_separated)>1 else "Plain text"
+
+                dataset_files.append({'id_in_source':file_id, 'name':file_name, 'size':file_size, 'file_type':file_type})
+
+            entry['source_id'] = self.repo
             entries.append(entry)
-        return entries
+            files.append(dataset_files)
+
+        return entries, files
 
 
 # if __name__ == "__main__":
